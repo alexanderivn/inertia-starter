d183a16c9bb59ed56c35741c94cbc170b8778600
Alexander Ivan <alexandrivn@proton.me>
2023-08-15 00:58:04 +0700

    Update Index.vue

--- resources/js/Pages/Front/Index.vue
+++ resources/js/Pages/Front/Index.vue
@@ -3,16 +3,33 @@
 </script>
 
 <template>
-    <h1>Inertia JS Starter</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad commodi corporis dolorem eligendi error impedit iure
-        iusto molestiae nesciunt nostrum!</p>
+    <section class="container mx-auto h-screen w-screen ">
+        <h1 class="text-4xl">Inertia JS Starterkit</h1>
+        <section>
+            <p>This is custom Inertia JS starterkit, this included:</p>
+        </section>
 
-    <ul>
-        <li>Laravel 10</li>
-        <li>Tailwind CSS</li>
-        <li>Inertia JS SSR</li>
-        <li>Ziggy</li>
-    </ul>
+        <section class="mt-8">
+            <ul class="list-disc">
+                <li>Laravel 10</li>
+                <li>Tailwind CSS</li>
+                <li>Inertia JS SSR</li>
+                <li>Ziggy</li>
+                <li>Fortify</li>
+                <li>Spatie Permission</li>
+                <li>Login/Register Page</li>
+                <li>Dashboard</li>
+
+            </ul>
+
+            <section class="mt-8">
+                <h4>Next thing to add:</h4>
+                <ul class="list-disc">
+                    <li>Forgot Password + Send email</li>
+                </ul>
+            </section>
+        </section>
+    </section>
 
     <a :href="route('about')">About Page</a>
 </template>
